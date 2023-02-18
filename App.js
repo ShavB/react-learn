817b6d32d21e2f27bb9e0ca17805fe3057a29bdc
Shyam Bisht <bishtshyam135@gmail.com>
2023-02-19 00:18:51 +0530

    1. Added header component 2. Added Assets folder

--- App.js
+++ App.js
@@ -1,13 +1,84 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client";
+import "./App.css";
+import cart from "./Assets/trolley.png";
+import mainLogo from "./Assets/diet.png";
 
-const header = React.createElement(
-  "h1",
-  {
-    id: "header-one",
-  },
-  "This is the first header"
-);
+// console.log(container);
+/**
+ * Header
+ *  - logo - done
+ *  - nav items - done  
+ *  - cart icon - done
+ *
+ * Body - 
+ *  - restaurent cards
+ *      - image
+ *      - name
+ *      - cuisine
+ * Footer
+ *  -review email
+ *
+ */
+
+const Header = () => {
+
+  // const [stickyHeader, setStickyHeader] = useState(false);
+
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
+
+  // const handleScroll =()=>{
+  //   if(window.scrollY > 0){
+  //     setStickyHeader(true);
+  //   }else{
+  //     setStickyHeader(false);
+  //   }
+  // }
+
+  return (
+    <>
+      <div className="main-header">
+        <div className="logo-icon">
+          <a href="/">
+            <img src={mainLogo} alt="website logo" />
+          </a>
+        </div>
+        <div className="nav-items-list">
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>User</li>
+          </ul>
+        </div>
+        <div className="cart">
+          <img src={cart} alt="cart-logo" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Body = () => {
+  return(
+    <div className="body-container">
+      {/* <h1>HEllo</h1> */}
+    </div>
+  );
+}
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(<AppLayout />);
